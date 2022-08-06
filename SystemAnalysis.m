a32146370d38eee60335ff2e803ca5cac17efa2a
Toren Arginteanu <torenarginteanu@Torens-MacBook-Pro.local>
2022-08-06 17:38:01 -0400

    unknown

--- SystemAnalysis.m
+++ SystemAnalysis.m
@@ -93,9 +93,9 @@ clear H01 P04
 [fcn, yname, ylims] = MeasurementSelector();
 
 %%
-[Y,t] = fcn(H01_Epoch_w_PP, H01_Epoch_t_PP); t = t(:,1);
-BL = fcn(H01_w_BL, H01_t_BL); Y = Y - BL;
-chloc = H01_t_BL.chanlocs;
+[Y,t] = fcn(P04_Epoch_w_PP, P04_Epoch_t_PP); t = t(:,1);
+BL = fcn(P04_w_BL, P04_t_BL); Y = Y - BL;
+chloc = P04_t_BL.chanlocs;
 %Y = Y(:,14); chloc = chloc(14); % cz
 
 %{
@@ -104,9 +104,9 @@ flt = designfilt('highpassiir', 'SampleRate', 1/mean(diff(t)), ...
 Y = filtfilt(flt, Y);
 %}
 %%
-trl = 2;
-tsplit = H01_boundTimes_PP(trl,:);
-tt = [H01_event_PP(tsplit).latency]/500;
+trl = 4;
+tsplit = P04_boundTimes_PP(trl,:);
+tt = [P04_event_PP(tsplit).latency]/500;
 hbnd = tt(1:2) + [0,-.5]; ybnd = tt(2:3);
 h_idx = (t <= hbnd(2)) & (t >= hbnd(1));
 y_idx = (t <= ybnd(2)) & (t >= ybnd(1));
@@ -114,10 +114,10 @@ Yh = Y(h_idx,:); Yy = Y(y_idx,:);
 th = t(h_idx);   ty = t(y_idx);
 
 figure; plot(th, Yh); xlabel('time (s)'); ylabel(yname);
-title(['Subject H01 PinPrick Trial ',num2str(trl),' - Impulse Response']);
+title(['Subject P04 PinPrick Trial ',num2str(trl),' - Impulse Response']);
 
 ypred = zeros(size(Yy));
-tt = [H01_event_PP(tsplit(2):tsplit(3)).latency]/500;
+tt = [P04_event_PP(tsplit(2):tsplit(3)).latency]/500;
 tt = tt - ty(1); ty = ty - ty(1); th = th - th(1);
 %figure; hold on;
 for tDelta = tt
@@ -142,7 +142,7 @@ subplot(2,1,2); plot(ty, Yy); hold on; plot(ty, ypred);
 %}
 
 %%
-sttl = ['Subject H01 PinPrick Trial ',num2str(trl),' - ',yname];
+sttl = ['Subject P04 PinPrick Trial ',num2str(trl),' - ',yname];
 fig(2) = figure; sgtitle(sttl);
 fig(1) = figure; sgtitle(sttl);
 H = floor(sqrt(size(Yy,2)));
