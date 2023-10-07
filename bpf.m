b435630050a8bbb3ebb229adcc98291e2c51438e
Toren Arginteanu <torenarginteanu@Torens-MacBook-Pro.local>
2023-10-07 12:37:28 -0400

    removed unused example code

--- bpf.m
+++
@@ -1,7 +0,0 @@
-function filt_signal = bpf(signal, low_thresh,high_thresh,fs)
-    n           = 5;   % Butterworth filter order
-    range       = fs/2;
-    wn          = [low_thresh high_thresh]/range; 
-    [b,a]       = butter(n,wn);
-    filt_signal = filtfilt(b,a,signal);
-end
