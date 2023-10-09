55004783854f99a0df68b2ef1a6b3ee2afef7ac5
Toren Arginteanu <torenarginteanu@Torens-MacBook-Pro.local>
2023-10-09 15:49:16 -0400

    git status REWROTE TO USE REORGANIZED TABLE git status

--- fcnTbl.m
+++ fcnTbl.m
@@ -1,30 +1,30 @@
-function [tblOut, eegTbl, epochTbl, epochSpecTbl] = ...
-    fcnTbl(eegTbl, epochTbl, epochSpecTbl, fcn, vars)
+function [tblOut, tblIn] = ...
+    fcnTbl(tblIn, fcn, vars)
     makeSubtbl = @(tbl, vars) tbl(:, ismember(tbl.Properties.VariableNames, vars));
 
     if nargin > 4
-        eegTbl       = makeSubtbl(eegTbl,       vars);
-        epochTbl     = makeSubtbl(epochTbl,     vars);
-        epochSpecTbl = makeSubtbl(epochSpecTbl, vars);
+        tblIn = makeSubtbl(tblIn, vars);
     end
     
-    tblOut = epochTbl; 
-    W = height(epochTbl); H = width(epochTbl); 
-    for c = 1:H
-        for r = 1:W
-            curSpecs = epochSpecTbl{r,c}{:};
-            curEpocs = epochTbl{r,c}{:};
-            %Y = cell(size(curEpocs)); t = cell(size(Y));
-            tY = cell(size(curEpocs));
-            for trl = 1:length(curEpocs)
-                curSpec = curSpecs{trl};
-                curEpoc = curEpocs{trl};
-                if ~isempty(curEpoc)
-                    [Y_trl,t_trl] = fcn(curSpec, curEpoc);
-                    tY{trl} = cat(3,t_trl,Y_trl);
-                end
+    tblSpec = tblIn(4,:);
+    tblEpoc = tblIn(3,:);
+    tblOut = tblEpoc; tblOut.Properties.RowNames = {'tY'};
+    W = width(tblIn); 
+    for c = 1:W
+        curSpecs = tblSpec{1,c}{:};
+        curEpocs = tblEpoc{1,c}{:};
+        %Y = cell(size(curEpocs)); t = cell(size(Y));
+        tY = cell(size(curEpocs));
+        for trl = 1:length(curEpocs)
+            curSpec = curSpecs{trl};
+            curEpoc = curEpocs{trl};
+            if ~isempty(curEpoc)
+                [Y_trl,t_trl] = fcn(curSpec, curEpoc);
+                tY{trl} = cat(3,t_trl,Y_trl);
             end
-            tblOut{r,c} = tY;
         end
+        tblOut{1,c} = {tY};
     end
+
+    tblOut = [tblIn; tblOut];
 end
